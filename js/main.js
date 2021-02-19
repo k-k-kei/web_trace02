cea3ab6a355d4d21c7b468f8953f8376a2da8be9
k-k-kei <42371057+k-k-kei@users.noreply.github.com>
2021-02-19 22:46:53 +0900

    add files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+	$('.slider').slick({
+    autoplay:true,
+    autoplaySpeed:5000,
+    arrows: false,
+    fade: true,
+    speed: 800,
+    cssEase: 'cubic-bezier(.645, .045, .355, 1)'
+  });
+});
